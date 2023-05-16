8bd8b96c38c690faf812e4e5fe335a3654d6c2fb
ShuOby2510 <131288216+ShuOby2510@users.noreply.github.com>
2023-05-16 17:02:28 +0900

    Add files via upload

---
+++ easylogin/easylogin-1/src/main/java/jp/co/internous/easylogin/Easylogin1Application.java
@@ -0,0 +1,13 @@
+package jp.co.internous.easylogin;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Easylogin1Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Easylogin1Application.class, args);
+	}
+
+}
